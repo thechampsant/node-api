ca6d6feb2ce8cb14cd58eaaf8f217138423468f4
Santosh Kumar Prasad <santoshprsd71@gmail.com>
2018-12-27 15:11:25 +0530

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true },(err, client)=>{
+    if(err){
+        return console.log('we are unable to connect to MongoDB server');
+    }
+    console.log('Connected to mongoDB server');
+//    const db = client.db('TodoApp'); //creating a database reference
+//    
+//    db.collection('Todos').insertOne({
+//        text: 'This is mongo ',
+//        completed: false
+//    },(err,result)=>{
+//        if(err){
+//            return console.log('unable to insert todo',er0r);
+//        }
+//        console.log(JSON.stringify(result.ops,undefined,2));
+//    });
+//    
+//    db.collection('Users').insertOne({
+//        name: 'matt',
+//        age : 24,
+//        location: 'Kolkata'
+//    },(err,result)=>{
+//        if(err){
+//            return console.log('unable to insert user',err);
+//        }
+//        console.log(JSON.stringify(result.ops,undefined,2));
+//    });
+//    
+    client.close();
+}); 
+
+//this method takes two arguments 1st parameter will be a string, in production example this would the url where your                            database lives like AWS, Azure, or horuku url and stuff like that. 2nd argument is a callback func which will fire up                            after the connection made is succeeded or failed.
